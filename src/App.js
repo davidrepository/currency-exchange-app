cbab373390f7bb0168abed621763959a7463b964
davidrepository <dawidxpluciennik@gmail.com>
2020-08-03 20:39:13 +0200

    Add components folder, CurrencyInputs component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import CurrencyInputs from './components/CurrencyInputs';
 
 const App = () => {
   return (
     <div className="App">
       <h1>Hello App</h1>
+      <CurrencyInputs />
     </div>
   );
 };
---
+++ src/components/CurrencyInputs.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const CurrencyInputs = () => {
+  return (
+    <div className="CurrencyInputs">
+      <h1>Hello CurrencyInputs</h1>
+      <input></input>
+      <select>
+        <option>Option</option>
+      </select>
+    </div>
+  );
+};
+
+export default CurrencyInputs;
